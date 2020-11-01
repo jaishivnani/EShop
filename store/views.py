b47490897bddda5363629ed13396312d69e3b7d0
Jai Shivnani <jaishivnani30@gmail.com>
2020-11-01 21:07:56 +0530

    first commit

---
+++ store/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models.product import Product
+
+# Create your views here.
+
+
+def index(request):
+     prods = Product.get_all_products();
+
+     return render(request,'index.html',{'products':prods})
